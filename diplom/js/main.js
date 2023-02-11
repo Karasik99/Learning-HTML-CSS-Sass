17a44fa5861323239281d07316817fa639c68be9
Karasya <nikita.carasevitch@yandex.by>
2023-02-11 20:23:54 +0300

    commit
    Доделал секцию достижений, используя флексы, также сделал секцию с слайдером, подключил js

---
+++ diplom/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true ,
+        dots: false,
+        autoplay : false,
+        autoplaySpeed: 2000,
+    });
+});
