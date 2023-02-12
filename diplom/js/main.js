7dfe71d2b45b4a856201cd7bd334f01a594aa2be
Karasya <nikita.carasevitch@yandex.by>
2023-02-12 16:45:08 +0300

    Commit
    Сделал еще один слайдер, добавил плавный скрол, сверстал две секции

--- diplom/js/main.js
+++ diplom/js/main.js
@@ -5,7 +5,29 @@ $(function(){
         slidesToScroll: 1,
         arrows: true ,
         dots: false,
-        autoplay : false,
+        // autoplay : true,
         autoplaySpeed: 2000,
+    });
+});
+
+$(function(){
+    $('.slider-2').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true ,
+        dots: false,
+        // autoplay : true,
+        autoplaySpeed: 2000,
+    });
+});
+
+document.querySelectorAll('a[href^="#servises"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
